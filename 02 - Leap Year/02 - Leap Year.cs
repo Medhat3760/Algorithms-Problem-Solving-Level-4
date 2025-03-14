485dc777918e67263d7885a067ec20ef1562c203
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-03-14 16:09:29 +0200

    Added solution for Problem #02 Check Leap Year

---
+++ 02 - Leap Year/02 - Leap Year.cs
@@ -0,0 +1,40 @@
+﻿namespace _02___Leap_Year
+{
+    internal class Program
+    {
+
+        static short ReadYear()
+        {
+
+            short year = 0;
+
+            Console.Write("Please enter a year to check ? ");
+            year = short.Parse(Console.ReadLine());
+            return year;
+
+        }
+
+        static bool IsLeapYear(short year)
+        {
+
+            return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            short year = ReadYear();
+
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine($"\nYes, year [ {year} ] is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"\nNo, year [ {year} ] is NOT a leap year.");
+            }
+
+        }
+    }
+}
