9cfe3c9b1ac110b6195e934d816ca2e3a945cd4f
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-03-20 03:11:01 +0200

    Added a solution for Problem #06

---
+++ 06 - Number Of Days In A Month - Short Logic/06 - Number Of Days In A Month - Short Logic.cs
@@ -0,0 +1,58 @@
+﻿namespace _06___Number_Of_Days_In_A_Month___Short_Logic
+{
+    internal class Program
+    {
+
+        static short ReadYear()
+        {
+
+            short year = 0;
+
+            Console.Write("\nPlease enter a year to check? ");
+            year = short.Parse(Console.ReadLine());
+            return year;
+
+        }
+
+        static short ReadMonth()
+        {
+
+            short month = 0;
+
+            Console.Write("\nPlease enter a month to check? ");
+            month = short.Parse(Console.ReadLine());
+            return month;
+
+        }
+
+        static bool IsLeapYear(short year)
+        {
+            return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        }
+
+        static short NumberOfDaysInAMonth(short month, short year)
+        {
+
+            if (month < 1 || month > 12)
+            {
+                return 0;
+            }
+
+            short[] arrNumberOfDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            return (month == 2) ? (IsLeapYear(year) ? (short)29 : (short)28) : arrNumberOfDays[month - 1];
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            short year = ReadYear();
+
+            short month = ReadMonth();
+
+            Console.WriteLine($"\nNumber of days in month [{month}] is {NumberOfDaysInAMonth(month, year)}");
+
+        }
+    }
+}
