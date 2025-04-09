824db467d5e2e1d47959afab1d91f0560c28a1c6
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-04-09 23:08:04 +0200

    Added a solution for Problem #11

---
+++ 11 - Date From Day Order In A Year/11 - Date From Day Order In A Year.cs
@@ -0,0 +1,141 @@
+﻿namespace _11___Date_From_Day_Order_In_A_Year
+{
+    internal class Program
+    {
+
+        static short ReadDay()
+        {
+
+            short day = 0;
+
+            Console.Write("\nPlease enter a Day? ");
+            day = short.Parse(Console.ReadLine());
+
+            return day;
+
+        }
+
+        static short ReadMonth()
+        {
+
+            short month = 0;
+
+            Console.Write("\nPlease enter a Month? ");
+            month = short.Parse(Console.ReadLine());
+
+            return month;
+
+        }
+
+        static short ReadYear()
+        {
+
+            short year = 0;
+
+            Console.Write("\nPlease enter a year? ");
+            year = short.Parse(Console.ReadLine());
+
+            return year;
+
+        }
+
+        static bool IsLeapYear(short year)
+        {
+            return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        }
+
+        static short NumberOfDaysInAMonth(short month, short year)
+        {
+
+            if (month < 1 || month > 12)
+            {
+                return 0;
+            }
+
+            short[] arrNumberOfDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            return (month == 2) ? (IsLeapYear(year) ? (short)29 : (short)28) : arrNumberOfDays[month - 1];
+
+        }
+
+        static short NumOfDaysFromBeginningTheYear(short year, short month, short day)
+        {
+
+            short numOfDays = 0;
+
+            for (short i = 1; i < month; i++)
+            {
+
+                numOfDays += NumberOfDaysInAMonth(i, year);
+
+            }
+
+            numOfDays += day;
+
+            return numOfDays;
+
+        }
+
+        struct sDate
+        {
+
+            public short year;
+            public short month;
+            public short day;
+
+        }
+
+        static sDate GetDateFromDayOrderInAYear(short dayOrderInAYear, short year)
+        {
+
+            sDate date;
+
+            short remainingDays = dayOrderInAYear;
+            short monthDays = 0;
+
+            date.year = year;
+            date.month = 1;
+
+            while (true)
+            {
+
+                monthDays = NumberOfDaysInAMonth(date.month, year);
+
+                if (remainingDays > monthDays)
+                {
+                    remainingDays -= monthDays;
+                    date.month++;
+                }
+                else
+                {
+                    date.day = remainingDays;
+                    break;
+                }
+
+            }
+
+            return date;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            short day = ReadDay();
+
+            short month = ReadMonth();
+
+            short year = ReadYear();
+
+            short dayOrderInAYear = NumOfDaysFromBeginningTheYear(year, month, day);
+
+
+            Console.WriteLine("\nNumber Of Days From The Beginning Of The Year is " + dayOrderInAYear);
+
+            sDate date = GetDateFromDayOrderInAYear(dayOrderInAYear, year);
+
+            Console.WriteLine($"\nDate for [{dayOrderInAYear}] is: {date.day}/{date.month}/{date.year}");
+
+        }
+    }
+}
