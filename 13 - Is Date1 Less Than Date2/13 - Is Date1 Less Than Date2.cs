b8bff062d60820587186a61ec83e76ee977a511a
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-04-11 22:11:54 +0200

    Added a solution for Problem #13

---
+++ 13 - Is Date1 Less Than Date2/13 - Is Date1 Less Than Date2.cs
@@ -0,0 +1,93 @@
+﻿namespace _13___Is_Date1_Less_Than_Date2
+{
+    internal class Program
+    {
+
+        struct sDate
+        {
+
+            public short year;
+            public short month;
+            public short day;
+
+        }
+
+        static short ReadDay()
+        {
+
+            short day;
+
+            Console.Write("\nPlease enter a Day? ");
+            day = short.Parse(Console.ReadLine());
+
+            return day;
+
+        }
+
+        static short ReadMonth()
+        {
+
+            short month;
+
+            Console.Write("\nPlease enter a Month? ");
+            month = short.Parse(Console.ReadLine());
+
+            return month;
+
+        }
+
+        static short ReadYear()
+        {
+
+            short year;
+
+            Console.Write("\nPlease enter a Year? ");
+            year = short.Parse(Console.ReadLine());
+
+            return year;
+
+        }
+
+        static sDate ReadFulDate()
+        {
+
+            sDate date;
+
+            date.day = ReadDay();
+            date.month = ReadMonth();
+            date.year = ReadYear();
+
+            return date;
+
+        }
+
+        static bool IsDate1BeforeDate2(sDate date1, sDate date2)
+        {
+
+            return (date1.year < date2.year) ? true : ((date1.year == date2.year) ? ((date1.month < date2.month) ? true : ((date1.month == date2.month) ? (date1.day < date2.day) : false)) : false);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            sDate date1, date2;
+
+            date1 = ReadFulDate();
+
+            Console.WriteLine();
+
+            date2 = ReadFulDate();
+
+            if (IsDate1BeforeDate2(date1, date2))
+            {
+                Console.WriteLine("\n Yes, Date1 is less than Date2.");
+            }
+            else
+            {
+                Console.WriteLine("\n No, Date1 is NOT less than Date2.");
+            }
+
+        }
+    }
+}
