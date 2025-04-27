4d8ba739e713074856c3f0c3271e046e07ba6427
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-04-27 15:45:04 +0300

    Added a solution for Problems From #47 to #53

---
+++ 47 To 53 - More Date Problems/47 To 53 - More Date Problems.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+namespace _47_To_53___More_Date_Problems
+{
+    internal class Program
+    {
+
+        struct stDate
+        {
+
+            public short year;
+            public short month;
+            public short day;
+
+        }
+
+        static stDate GetSystemDate()
+        {
+
+            stDate date;
+            DateTime now = DateTime.Now;
+
+            date.day = (short)now.Day;
+            date.month = (short)now.Month;
+            date.year = (short)now.Year;
+
+            return date;
+
+        }
+
+        static short GetDayOfWeekOrder(short day, short month, short year)
+        {
+
+            short d, a, y, m;
+
+            a = (short)((14 - month) / 12);
+            y = (short)(year - a);
+            m = (short)(month + 12 * a - 2);
+            // Gregorian:
+            //0:sun, 1:Mon, 2:Tue...etc
+            d = (short)((day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7);
+
+            return d;
+
+        }
+
+        static short GetDayOfWeekOrder(stDate date)
+        {
+
+            return GetDayOfWeekOrder(date.day, date.month, date.year);
+
+        }
+
+        static string GetDayShortName(short dayOfWeekOrder)
+        {
+
+            string[] arrDayNames = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            return arrDayNames[dayOfWeekOrder];
+
+        }
+
+        static bool IsEndOfWeek(stDate date)
+        {
+            return GetDayOfWeekOrder(date) == 6;
+        }
+
+        static bool IsWeekEnd(stDate date)
+        {
+            //Weekends are Fri and Sat
+
+            short dayIndex = GetDayOfWeekOrder(date);
+
+            return dayIndex == 5 || dayIndex == 6;
+
+        }
+
+        static bool IsBusinessDay(stDate date)
+        {
+            //Business Days are Sun,Mon,Tue,Wed and Thur
+            /* short DayIndex = DayOfWeekOrder(Date);
+            return (DayIndex >= 0 && DayIndex <= 4);
+            */
+
+            //shorter method is to invert the IsWeekEnd: this will save updating code.           
+            return !IsWeekEnd(date);
+
+        }
+
+        static short GetDaysUntilEndOfWeek(stDate date)
+        {
+
+            return (short)(6 - GetDayOfWeekOrder(date));
+
+        }
+
+        static bool IsLeapYear(short year)
+        {
+
+            return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        }
+
+        static short NumberOfDaysInAMonth(short month, short year)
+        {
+
+            if (month < 1 || month > 12)
+            {
+                return 0;
+            }
+
+            short[] arrNumberOfDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            return (month == 2) ? ((short)(IsLeapYear(year) ? 29 : 28)) : arrNumberOfDays[month - 1];
+
+        }
+
+        static short GetDaysUntilEndOfMonth(stDate date)
+        {
+
+            return (short)(NumberOfDaysInAMonth(date.month, date.year) - date.day);
+
+        }
+
+        static short NumberOfDaysFromBeginningTheYear(stDate date)
+        {
+
+            short days = 0;
+
+            for (short i = 1; i < date.month; i++)
+            {
+
+                days += NumberOfDaysInAMonth(i, date.year);
+
+            }
+
+            days += date.day;
+
+            return days;
+
+        }
+
+
+        static short GetDaysUntilEndOfYear(stDate date)
+        {
+
+            short yearDays = (short)(IsLeapYear(date.year) ? 366 : 365);
+
+            return (short)(yearDays - NumberOfDaysFromBeginningTheYear(date));
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            stDate date1 = GetSystemDate();
+
+            string dayName = GetDayShortName(GetDayOfWeekOrder(date1));
+
+            Console.WriteLine($"\nToday is {dayName} , {date1.day}/{date1.month}/{date1.year}");
+
+            Console.WriteLine("\nIs it End of Week?");
+
+            if (IsEndOfWeek(date1))
+            {
+                Console.WriteLine("Yes it is Saturday, it's of Week.");
+            }
+            else
+            {
+                Console.WriteLine("No NOT end of week.");
+            }
+
+            Console.WriteLine("\nIs it Weekend?");
+
+            if (IsWeekEnd(date1))
+            {
+                Console.WriteLine("Yes it is a week end.");
+            }
+            else
+            {
+                Console.WriteLine($"No today is {dayName}, NOT a weekend.");
+            }
+
+            Console.WriteLine("\nIs it a Business Day?");
+
+            if (IsBusinessDay(date1))
+            {
+                Console.WriteLine("Yes it is a business day.");
+            }
+            else
+            {
+                Console.WriteLine("No it is NOT a business day.");
+            }
+
+            Console.WriteLine($"\nDays until end of week : {GetDaysUntilEndOfWeek(date1)} Day(s)");
+
+            Console.WriteLine($"\nDays until end of month : {GetDaysUntilEndOfMonth(date1)} Day(s)");
+
+            Console.WriteLine($"\nDays until end of year : {GetDaysUntilEndOfYear(date1)} Day(s)");
+
+        }
+    }
+}
