bacbdeaa3a699c88d2040c4fc1ccb08f7b81cae5
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-05-01 19:40:47 +0300

    Added a solution for Problem #57

---
+++ 57 - Compare Date Function/57 - Compare Date Function.cs
@@ -0,0 +1,117 @@
+﻿namespace _57___Compare_Date_Function
+{
+    internal class Program
+    {
+
+        struct stDate
+        {
+
+            public short year;
+            public short month;
+            public short day;
+
+        }
+
+        static short ReadDay()
+        {
+
+            short day;
+
+            Console.Write("Please enter a Day? ");
+            day = short.Parse(Console.ReadLine());
+
+            return day;
+
+        }
+
+        static short ReadMonth()
+        {
+
+            short month;
+
+            Console.Write("Please enter a Month? ");
+            month = short.Parse(Console.ReadLine());
+
+            return month;
+
+        }
+
+        static short ReadYear()
+        {
+
+            short year;
+
+            Console.Write("Please enter a Year? ");
+            year = short.Parse(Console.ReadLine());
+
+            return year;
+
+        }
+
+        static stDate ReadFulDate()
+        {
+
+            stDate date;
+
+            date.day = ReadDay();
+            date.month = ReadMonth();
+            date.year = ReadYear();
+
+            return date;
+
+        }
+
+        static bool IstDate1BeforeDate2(stDate date1, stDate date2)
+        {
+
+            if (date1.year != date2.year)
+                return date1.year < date2.year;
+
+            if (date1.month != date2.month)
+                return date1.month < date2.month;
+
+            return date1.day < date2.day;
+
+        }
+
+        static bool IstDate1EqualDate2(stDate date1, stDate date2)
+        {
+
+            return (date1.year == date2.year && date1.month == date2.month && date1.day == date2.day);
+
+        }
+
+        enum enDateCompare { Before = -1, Equal = 0, After = 1 }
+
+        static enDateCompare CompareDates(stDate date1, stDate date2)
+        {
+            //return IstDate1BeforeDate2(date1, date2) ? enDateCompare.Before : (IstDate1EqualDate2(date1, date2) ? enDateCompare.Equal : enDateCompare.After);
+
+            if (IstDate1BeforeDate2(date1, date2))
+            {
+                return enDateCompare.Before;
+            }
+
+            if (IstDate1EqualDate2(date1, date2))
+            {
+                return enDateCompare.Equal;
+            }
+
+            return enDateCompare.After;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\nEnter Date1:");
+            stDate date1 = ReadFulDate();
+
+            Console.WriteLine("\nEnter Date2:");
+            stDate date2 = ReadFulDate();
+
+            Console.WriteLine("\nCompare Result = " + CompareDates(date1, date2));
+
+        }
+    }
+}
