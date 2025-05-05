6604ec29cdbf15ddb8d817b0ef6476f7ad96f56b
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-05-06 01:19:57 +0300

    Added a solution for Problem #63 To #64

---
+++ 63 To 64 - Read-Print Date String/63 To 64 - Read-Print Date String.cs
@@ -0,0 +1,89 @@
+﻿namespace _63_To_64___Read_Print_Date_String
+{
+    internal class Program
+    {
+
+        struct stDate
+        {
+
+            public short year;
+            public short month;
+            public short day;
+
+        }
+
+        static string ReadStringDate(string message)
+        {
+
+            Console.Write(message);
+            string date = Console.ReadLine();
+
+            return date;
+
+        }
+
+        static List<string> SplitString(string text, string delim)
+        {
+
+            List<string> lWords = new List<string>();
+
+            string sWord = "";
+            short pos = 0;
+
+            while ((pos = (short)text.IndexOf(delim)) != -1)
+            {
+
+                sWord = text.Substring(0, pos);
+
+                if (sWord != "")
+                {
+                    lWords.Add(sWord);
+                }
+                text = text.Substring(pos + delim.Length);
+
+            }
+
+            if (text != "")
+            {
+                lWords.Add(text);
+            }
+
+            return lWords;
+        }
+
+        static stDate StringToDate(string dateString)
+        {
+
+            stDate date;
+
+            List<string> lDate = SplitString(dateString, "/");
+
+            date.day = short.Parse(lDate[0]);
+            date.month = short.Parse(lDate[1]);
+            date.year = short.Parse(lDate[2]);
+
+            return date;
+
+        }
+
+        static string DateToString(stDate date)
+        {
+            return date.day.ToString() + "/" + date.month.ToString() + "/" + date.year.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+
+            string dateString = ReadStringDate("\nPlease enter a Date dd/mm/yyyy? ");
+
+            stDate date = StringToDate(dateString);
+
+            Console.WriteLine("\nDay: " + date.day);
+            Console.WriteLine("Month: " + date.month);
+            Console.WriteLine("Year: " + date.year);
+
+            Console.WriteLine("\nYou Entered: " + DateToString(date));
+
+        }
+    }
+}
